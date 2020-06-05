a9fd17fa73d2271627f35602d8623dc83bfd7974
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-06-05 10:45:39 -0700

    chore: protect API keys with env variables

--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,10 @@
+// Protect API keys with env variables
+const dotenv = require('dotenv');
+dotenv.config();
+console.log(`Your Pixabay API key is ${process.env.PIXABAY_API_KEY}`);
+console.log(`Your Weatherbit API key is ${process.env.WEATHERBIT_API_KEY}`);
+console.log(`Your Geonames username is ${process.env.GEONAMES_USERNAME}`);
+
 const path = require('path');
 const express = require('express');
 const PORT = 8081;
@@ -17,6 +24,7 @@ app.use(cors());
 app.use(express.static('dist'))
 
 // Listen for connections
-app.listen(PORT, function () {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
