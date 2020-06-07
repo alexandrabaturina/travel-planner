0c3762ab10d6c026688aed3f957269ff88963592
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-06-07 14:57:49 -0700

    feat: api call to geonames

--- src/server/server.js
+++ src/server/server.js
@@ -5,11 +5,14 @@ console.log(`Your Pixabay API key is ${process.env.PIXABAY_API_KEY}`);
 console.log(`Your Weatherbit API key is ${process.env.WEATHERBIT_API_KEY}`);
 console.log(`Your Geonames username is ${process.env.GEONAMES_USERNAME}`);
 
+
 const path = require('path');
 const express = require('express');
 const PORT = 8081;
 
 const app = express()
+
+const http = require('http');
 
 // Configure express
 const bodyParser = require('body-parser');
@@ -28,3 +31,26 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
 
+app.post('/api', function (req, res) {
+    const placename = req.body.city;
+    console.log(placename);
+    const GEONAMES_API_ENDPOINT = `http://api.geonames.org/postalCodeSearchJSON?placename=${placename}&maxRows=10&username=${process.env.GEONAMES_USERNAME}`;
+    console.log(GEONAMES_API_ENDPOINT)
+
+    http.get(GEONAMES_API_ENDPOINT, (response) => {
+        let data = '';
+        // A chunk of data has been recieved.
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        response.on('end', () => {
+            res.send(JSON.parse(data))
+        });
+    })
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve('index.html'))
+})
