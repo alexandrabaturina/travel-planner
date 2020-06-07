5062679f395f38630495876064bb5fabbf5b8265
alexandrabaturina <alexandra.v.baturina@gmail.com>
2020-06-07 12:03:49 -0700

    chore: import styles

---
+++ src/client/index.js
@@ -0,0 +1,6 @@
+import './styles/image.scss'
+import './styles/main.scss'
+import './styles/trip-info.scss'
+import './styles/footer.scss'
+import './styles/header.scss'
+import './styles/form.scss'
